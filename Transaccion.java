1e2ea6afbdc76aa5ea799bc1bf054a8a6b4d6329
Jhon Cruz <jhonsebastian97@hotmail.es>
2022-08-31 20:50:54 -0500

    Transaccion
    Es la primera versión

---
+++ Transaccion.java
@@ -0,0 +1,62 @@
+
+package Proyecto;
+
+import java.util.Date;
+
+public class Transaccion {
+    private long id;
+    private String concept;
+    private float amount;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public Transaccion (long id, String concept, float amount, Date createdAt, Date updatedAt){
+        this.id = id;
+        this.concept = concept;
+        this.amount = amount;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getConcept() {
+        return concept;
+    }
+
+    public void setConcept(String concept) {
+        this.concept = concept;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    
+    
+}
