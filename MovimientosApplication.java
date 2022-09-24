08d7ef665c946dc1d50b71d80535119b1a6e6a33
sebastiancs95 <jhonsebastian97@hotmail.es>
2022-09-24 18:57:40 -0500

    Archivos faltantes
    Adm empresas

---
+++ MovimientosApplication.java
@@ -0,0 +1,13 @@
+package com.Neteam.Movimientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovimientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovimientosApplication.class, args);
+	}
+
+}
