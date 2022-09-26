6bf1032f09f9f226b7b774af0f2693951fd8fb4b
aisabel14 <111585015+aisabel14@users.noreply.github.com>
2022-09-26 22:21:40 +0200

    Update FlujoDeCajaApplication.java

--- src/main/java/com/proyecto/FlujoDeCaja/FlujoDeCajaApplication.java
+++ src/main/java/com/proyecto/FlujoDeCaja/FlujoDeCajaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@ComponentScan
+
 public class FlujoDeCajaApplication {
 
 	public static void main(String[] args) {
