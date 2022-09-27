66b11a87cd14e548955480ad9e4b0e68997d5cc4
aisabel14 <aisabelperezd14@gmail.com>
2022-09-27 10:40:38 +0200

    fisrt commit

--- src/main/java/com/proyecto/FlujoDeCaja/FlujoDeCajaApplication.java
+++ src/main/java/com/proyecto/FlujoDeCaja/FlujoDeCajaApplication.java
@@ -3,6 +3,7 @@ package com.proyecto.FlujoDeCaja;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 
 public class FlujoDeCajaApplication {
